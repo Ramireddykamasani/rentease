4a8201c64a92538dda6d4678a6ac5370ab4d6225
ramireddy <ramireddy15201999@gmail.com>
2025-03-18 19:02:52 +0530

    first commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Home from './components/pages/home/Home'
+import Authlayout from './components/auth/authlayout/Authlayout'
+import Register from './components/auth/register/Register'
+import Login from './components/auth/login/Login'
 
 function App() {
 
   return (
-<Home/>
+    <Routes>
+      <Route path={"/auth"} element={<Authlayout/>}>
+        <Route  path="login" element={<Login/>} />
+        <Route  path="register" element={<Register/>} />
+      </Route>
+    </Routes>
   )
 }
 
